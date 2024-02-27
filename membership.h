bcad7537cb82163273924e52ef822f9ebb31a301
rene2412 <hrene2412@gmail.com>
2024-02-27 10:49:30 -0800

    save and load

--- membership.h
+++ membership.h
@@ -1,16 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <unordered_map>
-class Membership {
-private:
-   float fee;
-   float payment;
-public:
-   Membership(float f = 100, float p = 25) : fee(f), payment(p) {}
-
-   float GetFee() const { return fee; }
-   float GetPayment() const { return payment; }
- 
-};
 
 class Person {
 private:
@@ -20,7 +11,7 @@ private:
    std::string phone_number;
    bool account;
 public:
-    Person(std::string m = "", std::string n = "", int a = 0, std::string p = "", bool a) : membership_id(m), name(n), age(a), phone_number(p) account(a) {} 
+    Person(std::string m = "", std::string n = "", int a = 0, std::string p = "", bool b = false) : membership_id(m), name(n), age(a), phone_number(p), account(b) {} 
     
     std::string GetMemberShipId() const { return membership_id; } 
     std::string GetName() const { return name; }
@@ -34,15 +25,64 @@ public:
      void SetPhoneNumber(std::string &p) { phone_number = p; }
      void SetAccount(bool &b) { account = b; }
 
-     bool operator< (const Person &other) const {
-     	return name < other.name
-     }
-
 };
  
-class System : public Membership, public Person {
+class System : public Person {
 private: 
-     std::unordered_map<std::string key, Person> database; // will hold the enite gym database
+     std::unordered_map<std::string, Person> database; // will hold the entire gym database
 public:
-	
+     //key
+     void AddPerson(const Person &person) {
+	   database[person.GetMemberShipId()] = person;
+	} 
+     void PrintDataBase() const {
+	    for (const auto& pair : database) {
+	     std::cout << "Membership ID: " << pair.first << " | Name: " << pair.second.GetName() << 
+	     " | Age: " << pair.second.GetAge() << " | Phone Number: " << pair.second.GetPhoneNumber() << 
+	     " | Account Status: " << (pair.second.GetAccount() ? "Active" : "Inactive") << std::endl;
+	     }  
+	}
+    void remove() {} 	
+    void search() {}
+
+    void save_file(const std::string &filename) const {
+	std::ofstream file(filename);
+	if (file.is_open()) {
+	    for (const auto& pair : database) {
+		file << "Membership ID: " << pair.first << " | Name: " <<
+		pair.second.GetName() << " | Age: " << pair.second.GetAge() << 
+		" | Phone Number: " << pair.second.GetPhoneNumber() << " | Account Status: "
+		<< (pair.second.GetAccount() ? "Active" : "Inactive");
+	    }
+	}
+	else std::cout << "Error Opening File" << std::endl;
+	}
+    
+    void load_file(const std::string &filename) {
+	    std::ifstream file(filename);
+	    if (file.is_open()) {
+		std::string line;
+		while(std::getline(file, line)) {
+		    std::stringstream srs(line);
+		    std::string id, name, phone_number;
+		    int age = 0;
+		    bool account;
+		   // if reading in the data is valid
+		    if(std::getline(srs, id, " : ") and
+		    std::getline(srs, name, " : ") and
+		    (srs >> age) and (srs.ignore(), std::getline(srs, phone_number, " : "))
+		    and (srs >> account)) {
+		    // then create objects of each person
+		    Person newPerson(id, name, age, phone_number, account);
+		    database[id] = newPerson;
+		}
+		else std::cout << "Error Parsing Data" << std::endl;
+	    }
+	    file.close();
+	    }
+	    else std::cout << "Error opening file" << std::endl;
+	}
+
+
+
 };
